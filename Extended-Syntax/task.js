1d3e96e9c17a5b11dc6ec1165e8c125d8a09193d
Aikanush <haka.m@mail.ru>
2019-11-11 10:50:18 +0400

    task.js

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,40 +1,60 @@
 
+'use strict';
 
-function calculateQuadraticEquation(){
-    let a = +window.a.value;
-    let b = +window.b.value;
-    let c = +window.c.value;
-    let result = getResult(a,b,c);
-    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
-    let span = window.result;
-    span.textContent = "х = "+result;
+// Задание 1
+
+function getResult(a, b, c) {
+    let D = b**2 - 4*a*c;
+    let arr = [];
+
+    if (D > 0) {
+        arr[0] = (-b + Math.sqrt(D)) / (2*a);
+        arr[1] = (-b - Math.sqrt(D)) / (2*a);
+        return arr[0, 1];
+    }
+
+    else if (D = 0) {
+        arr[0] = -b / (2*a)
+        return arr[0]
+
+    }
+
+    else {
+        return arr;
+    }
+
 }
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
+console.log(getResult(2, 6, 5));
+
+// Задание 2
+
+function getAverageMark(marks) {
+  let total = 0;
+  if (marks.length > 5) {
+    marks.splice(5);
+    console.log(`Среднее значение первых 5 оценок:`);
+  }
+  for (let i = 0; i < marks.length; i++) {
+      total += marks[i];
+    }
+    console.log(total / marks.length);
 }
 
-function calculateAverageRating(){
-    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
-    let averageMark = getAverageMark(marks);
-    window.averageMark.textContent = averageMark;
-}
+getAverageMark([2, 4, 5, 4, 4, 5]);
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
-}
+// Задание 3
 
-function calculateDrinkTask(){
-    let name = window.personName.value;
-    let dateOfBirthday = new Date(window.dateOfBirthday.value);
-    let drink = askDrink(name, dateOfBirthday);
-    window.drink.textContent = drink;
+function askDrink(name, dateOfBirthday) {
+  let today = new Date().getFullYear();
+  let birthdayYear = new Date(dateOfBirthday).getFullYear();
+    let age = today - birthdayYear;
+  let text;
+    if (age >= 18) {
+        text = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        text = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+  return text;
 }
-
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
-}
+console.log(askDrink('Aykansuh', new Date(2005, 17, 10)));
