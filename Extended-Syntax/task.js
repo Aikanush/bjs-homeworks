c748788f76af4cfa324fe559fc7f40829c5b06c9
Aikanush <haka.m@mail.ru>
2019-11-11 11:14:42 +0400

    task.js

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -3,6 +3,16 @@
 
 // Задание 1
 
+function calculateQuadraticEquation(){
+    let a = +window.a.value;
+    let b = +window.b.value;
+    let c = +window.c.value;
+    let result = getResult(a,b,c);
+    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
+    let span = window.result;
+    span.textContent = "х = "+result;
+}
+
 function getResult(a, b, c) {
     let D = b**2 - 4*a*c;
     let arr = [];
@@ -28,6 +38,12 @@ function getResult(a, b, c) {
 
 // Задание 2
 
+function calculateAverageRating(){
+    let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
+    let averageMark = getAverageMark(marks);
+    window.averageMark.textContent = averageMark;
+}
+
 function getAverageMark(marks) {
   let total = 0;
   if (marks.length > 5) {
@@ -42,6 +58,13 @@ function getAverageMark(marks) {
 
 
 // Задание 3
+
+function calculateDrinkTask(){
+    let name = window.personName.value;
+    let dateOfBirthday = new Date(window.dateOfBirthday.value);
+    let drink = askDrink(name, dateOfBirthday);
+    window.drink.textContent = drink;
+}
 
 function askDrink(name, dateOfBirthday) {
   let today = new Date().getFullYear();
